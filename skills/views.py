2d8ab45801e99f760bb12ab154a09cb5ab9d7796
Carlos <domingues_santos@hotmail.com>
2024-01-24 17:01:10 -0300

    arquivos

---
+++ skills/views.py
@@ -0,0 +1,14 @@
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .models import Skill
+from .serializers import SkillSerializer
+
+
+class SkillList(APIView):
+    def get(self, request):
+        skills = Skill.objects.all()
+        serializer = SkillSerializer(skills,many=True)
+        return Response(serializer.data)
+
+    
+    
